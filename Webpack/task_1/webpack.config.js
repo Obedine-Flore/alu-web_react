e4e851ebd6daae5b778f1b0713110efa146f12a0
Obedine-Flore <f.obedine@alustudent.com>
2024-09-07 09:53:06 +0300

    task_2

---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './js/dashboard_main.js', // Entry file
+  output: {
+    filename: 'bundle.js', // Output file
+    path: path.resolve(__dirname, 'public'), // Output directory
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html' // Reference to the HTML file
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Use babel-loader for js files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production'
+};
