e3446f87790e9b4e6ad5206c1218df514c6050f9
Obedine-Flore <f.obedine@alustudent.com>
2024-09-07 10:07:53 +0300

    m

--- Webpack/task_0/webpack.config.js
+++ Webpack/task_0/webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'deploy')
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
